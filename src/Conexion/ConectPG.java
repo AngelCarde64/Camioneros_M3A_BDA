46c65ecfafc6be27455169c3f2eff976e6046659
AngelCarde64 <angelsteven2021@gmail.com>
2023-01-20 11:27:53 -0500

    Corregir la clase conectpg

---
+++ src/Conexion/ConectPG.java
@@ -0,0 +1,58 @@
+package Conexion;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConectPG {
+    //--> Para salvar o mantener viva la conexion
+    Connection con;
+    String cadenaConexion = "jdbc:oracle://localhost:5432/Proyecto_Programacion_Visual";
+    // ---> Se puede configurar para tener usuarios con menos privilegios, como que no puede borrar la base o tablas
+    String pgUsuario = "postgres";
+    String pgPassword = "angel654";
+
+    public ConectPG() {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConectPG.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            con = DriverManager.getConnection(cadenaConexion, pgUsuario, pgPassword);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConectPG.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ResultSet consulta(String sql) {
+        try {
+            //--> Crear setencia
+            Statement st = con.createStatement();
+            return st.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConectPG.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    // --> Puede ser un SQLException o un boolean
+    public boolean accion(String sql) {
+        try {
+            //Puede ser un INSERT-UPDATE-DELETE
+            Statement st = con.createStatement();
+            st.execute(sql);
+            st.close();//Cierrar la conexion.
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConectPG.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+}

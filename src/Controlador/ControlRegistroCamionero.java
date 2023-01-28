1bbee2dcde86eeb32e6f2bd174f40acf830840b6
AngelCarde64 <angelsteven2021@gmail.com>
2023-01-28 18:23:36 -0500

    Vista registro

---
+++ src/Controlador/ControlRegistroCamionero.java
@@ -0,0 +1,11 @@
+package Controlador;
+
+import Modelo.ModeloCamionero;
+import Vista.VistaRegistroCamionero;
+
+public class ControlRegistroCamionero {
+    
+    private VistaRegistroCamionero vistaRC;
+    private ModeloCamionero modeloCamionero;
+    
+}
